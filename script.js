73f42be6f7656ad5be84ef15d97ad93fd583f155
sayyaderfan <erfansayyad100@gmail.com>
2024-09-12 18:39:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const emojis = ['😍', '😍', '😎', '😎', '😛', '😛', '😨', '😨', '😇', '😇', '🤧', '🤧', '🤡', '🤡', '🤠', '🤠'];
+let shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+let displayMassege = document.getElementById('display');
+let display = document.querySelector('.massage');
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div');
+    box.className = "item";
+    box.innerHTML = shuf_emojis[i];
+
+    box.onclick = function () {
+        this.classList.add('boxOpen');
+
+        setTimeout(function () {
+            if (document.querySelectorAll('.boxOpen').length > 1) {
+                if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                        displayMassege.innerHTML = "You Win Game";
+                        display.style.display = "block"
+                    }
+                }
+                else {
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                }
+            }
+        }, 500)
+
+    }
+
+    document.querySelector('.game').appendChild(box);
+}
